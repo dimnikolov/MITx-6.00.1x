94adaa28e34afcdac6967972f103f4f6c1a977bc
dimnikolov <dimnikolov@gmail.com>
2017-11-28 15:13:05 +0200

    Count the number of occurences of a substring in a string
    This script counts the number of occurences of the substring "bob" in the string "alicebobobjohnsmithbart". The tricky part for me was to get the exact count despite the fact that the occurences overlap.

---
+++ Week 1/Occurence of substring in string/occurence-of-substring-in-string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 15:05:16 2017
+
+@author: dimitar.stoyanov
+"""
+
+s = 'alicebobobjohnsmithbart'
+
+total = 0
+for i in range(len(s)-2):
+    if s[i:i+3] == 'bob':
+        total += 1
+print ('The string is:' + ' ' + str(s))
+print ('Number of times that bob occurs in the string' + ' "' + str(s) + '" ' + 'is ' + str(total))
