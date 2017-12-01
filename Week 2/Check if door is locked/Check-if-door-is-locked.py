0638226e51d282f0ecf917dee274e2ff7aa51749
dimnikolov <dimnikolov@gmail.com>
2017-12-01 16:33:49 +0200

    Check if the door is locked
    Imagine that we have a smart door lock that returns a Boolean if the door is locked/unlocked. This script checks if the door is locked and prints two messages. One confirming that it is -- and another that prompts the user to lock the door.

---
+++ Week 2/Check if door is locked/Check-if-door-is-locked.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 11:24:00 2017
+
+@author: Dimitar Nikolov
+"""
+
+isLocked = True
+
+if isLocked is True:
+    print("The door is locked.")
+else:
+    print("Lock the door!")
