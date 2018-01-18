abfea33bf3ceec336f87a465bbf9c4b963271160
dimnikolov <dimnikolov@gmail.com>
2018-01-18 18:04:29 +0200

    Initial commit to Github

---
+++ Files and modules/circle.py
@@ -0,0 +1,6 @@
+pi = 3.14159
+
+def area(radius):
+    return pi*(radius**2)
+def circumference(radius):
+    return 2*pi*radius
---
+++ Files and modules/main.py
@@ -0,0 +1,5 @@
+# Imports the Circle.py module
+import circle
+
+# Prints the value of Pi as defined in the Circle.py module
+print(circle.pi)
